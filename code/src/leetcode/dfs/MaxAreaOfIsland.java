c6759e981a95f452390ee9b849f32a2a5c38be14
yyb345 <lyc@LYCdeMacBook-Pro.local>
2024-09-03 22:38:11 +0800

    dfs 最大区域

--- code/src/leetcode/dfs/MaxAreaOfIsland.java
+++ code/src/leetcode/dfs/MaxAreaOfIsland.java
@@ -1,38 +1,56 @@
 package leetcode.dfs;
 
+/**
+ * 题号 695
+ * [1,1,0,0,0],
+ * [1,1,0,0,0],
+ * [0,0,0,1,1],
+ * [0,0,0,1,1]
+ * 题目maxArea，也就是岛屿1最多的数量
+ */
 public class MaxAreaOfIsland {
 
-    int [][]direction=new int[][]{{0,-1},{0,1},{1,0},{-1,0}};
-    public int maxAreaOfIsland(int[][] grid) {
+    int[][] direction=new int[][]{{0,-1},{0,1},{1,0},{-1,0}};
 
-        //int value=Integer.MIN_VALUE;
+
+    int num=0;
+    public  int maxAreaOfIsland(int[][] grid) {
+
+        int max=0;
         boolean[][] visited=new boolean[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(!visited[i][j]){
-                    int value=DFS(grid,i,j,visited);
+                if(grid[i][j]==1 && !visited[i][j]){
+                    num=0;
+                    DFS(grid,i,j,visited);
+                    max=Math.max(max,num);
                 }
             }
         }
-        return 0;
+        return max;
     }
 
+    private void DFS(int[][] grid,int i,int j,boolean[][] visited){
 
-    public int DFS(int[][] grid,int i,int j,boolean[][] visited){
-        int result=1;
-        if(i>grid.length || j>grid[0].length )
-            return 0;
-        visited[i][j]=true;
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j])
+            return;
         if(grid[i][j]==0)
-            return 0;
+            return;
+
+        visited[i][j]=true;
+        num++;
 
         for(int k=0;k<direction.length;k++){
-            if(!visited[i+direction[k][0]][j+direction[k][1]]){
-                result+=DFS(grid,i+direction[k][0],j+direction[k][1],visited);
-            }
+            DFS(grid,i+direction[k][0],j+direction[k][1],visited);
         }
+    }
 
-        return result;
+    public static void main(String[] args) {
 
+        MaxAreaOfIsland maxAreaOfIsland = new MaxAreaOfIsland();
+        System.out.println(maxAreaOfIsland.maxAreaOfIsland(new int[][]{{1,1,0,0,0},{1,1,0,0,0},{0,0,0,1,1},{0,0,0,1,1}}));
+//        System.out.println(maxAreaOfIsland.maxAreaOfIsland(new int[][]{{1,0,1,1,1},{1,1,1,1,1}}));
+//        System.out.println(maxAreaOfIsland.maxAreaOfIsland(new int[][]{{1,0,1,1,1},{1,0,1,1,1}}));
+//        System.out.println(maxAreaOfIsland.maxAreaOfIsland(new int[][]{{0,0,0,0,0}}));
     }
 }
