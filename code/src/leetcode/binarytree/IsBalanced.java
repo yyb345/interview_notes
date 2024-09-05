0be567853ef1eab6940926e99e8d70898db2266c
yyb345 <lyc@LYCdeMacBook-Pro.local>
2024-09-05 11:08:16 +0800

    二叉树 平衡二叉树优化

--- code/src/leetcode/binarytree/IsBalanced.java
+++ code/src/leetcode/binarytree/IsBalanced.java
@@ -1,26 +1,37 @@
 package leetcode.binarytree;
 
 
-//TODO
+/** 110
+ * 判断是否是平衡二叉树
+ */
+
 public class IsBalanced {
 
-    public int maxDepth(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
-    }
 
     public boolean isBalanced(TreeNode root) {
 
+        return dfs(root)>-1;
+    }
+
+    int dfs(TreeNode root){
         if(root==null){
-            return true;
+            return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if(left==-1){
+            return -1;
+        }
+
+        if(right==-1){
+            return -1;
+        }
         if(Math.abs(left-right)>1){
-            return false;
+            return -1;
         }
-        return  isBalanced(root.left) && isBalanced(root.right);
+
+        return Math.max(left,right)+1;
     }
 }
