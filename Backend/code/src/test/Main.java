5647313796b978a03d4da71962df83c3d87befbb
yyb345 <lyc@LYCdeMacBook-Pro.local>
2024-10-02 20:11:40 +0800

    leetcode 191

--- Backend/code/src/test/Main.java
+++ Backend/code/src/test/Main.java
@@ -8,10 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Stack<String> stack = new Stack<>();
-        stack.push("123");
-        stack.push("88");
-        String join = String.join("/", stack);
-        System.out.println(join);
+
+        System.out.println(4>>1);
+        System.out.println(4<<1);
     }
 }
