b4f632319803ca96b1808d2a288be8a99a1c5a86
yyb345 <lyc@LYCdeMacBook-Pro.local>
2024-10-13 17:27:45 +0800

    API网关加密

--- Backend/code/test/problem/Main.java
+++ Backend/code/test/problem/Main.java
@@ -33,10 +33,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-       Stack<Integer> stack = new Stack<>();
-       stack.push(1);
-       stack.push(2);
-       System.out.println(stack.elementAt(0));
-        System.out.println(stack.elementAt(1));
+//       Stack<Integer> stack = new Stack<>();
+//       stack.push(1);
+//       stack.push(2);
+//       System.out.println(stack.elementAt(0));
+//        System.out.println(stack.elementAt(1));
+
+        List<Integer> list = new ArrayList<>();
+
     }
 }
